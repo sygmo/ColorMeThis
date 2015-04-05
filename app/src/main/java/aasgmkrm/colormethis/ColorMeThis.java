63d0b5203d2971f9088ec28abe291387ab6048ce
kevinrei <kevinrei@utexas.edu>
2015-04-05 00:16:02 -0500

    Fixed frequent crashing

--- app/src/main/java/aasgmkrm/colormethis/ColorMeThis.java
+++ app/src/main/java/aasgmkrm/colormethis/ColorMeThis.java
@@ -3,12 +3,8 @@ package aasgmkrm.colormethis;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.hardware.Camera;
-import android.hardware.Camera.CameraInfo;
 import android.hardware.Camera.PictureCallback;
-import android.hardware.Camera.Size;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.view.GestureDetectorCompat;
@@ -21,7 +17,8 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -29,7 +26,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 public class ColorMeThis extends Activity implements
         GestureDetector.OnGestureListener,
@@ -40,60 +36,32 @@ public class ColorMeThis extends Activity implements
 
     private String TAG = "Color Me This!";
 
+    /** Camera declarations. */
+    private Camera mCamera;
+    private CameraPreview mCameraPreview;
+    private Button mCaptureButton;
+
     // touch coordinates
     private float x;
     private float y;
 
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mCamera = getCameraInstance();
+        mCameraPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mCameraPreview);
 
-        // Instantiate gesture detector
-        mDetector = new GestureDetectorCompat(this, this);
-        mDetector.setOnDoubleTapListener(this);
-
-        // Create a RelativeLayout container that will hold a SurfaceView,
-        // and set it as the content of our activity.
-        mPreview = new Preview(this);
-        setContentView(mPreview);
-
-        // Find the total number of cameras available
-        numberOfCameras = Camera.getNumberOfCameras();
-
-        // Find the ID of the default camera
-        CameraInfo cameraInfo = new CameraInfo();
-        for (int i = 0; i < numberOfCameras; i++) {
-            Camera.getCameraInfo(i, cameraInfo);
-            if (cameraInfo.facing == CameraInfo.CAMERA_FACING_BACK) {
-                defaultCameraId = i;
+        mCaptureButton = (Button) findViewById(R.id.button_capture);
+        mCaptureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamera.takePicture(null, null, mPicture);
             }
-        }
-
-        /*       *//** Metering and focus areas. *//*
-
-        // Set Camera parameters.
-        Camera.Parameters params = mCamera.getParameters();
-
-        if (params.getMaxNumMeteringAreas() > 0) {
-            // Check that metering areas are supported.
-            List<Camera.Area> meteringAreas = new ArrayList<Camera.Area>();
-
-            //Specify an area in the center of the image.
-            Rect areaRect1 = new Rect(-100, -100, 100, 100);
-
-            // Set weight to 60%.
-            meteringAreas.add(new Camera.Area(areaRect1, 600));
-
-            // Specify area in the upper right corner of the image.
-            Rect areaRect2 = new Rect(800, -1000, 1000, 800);
-
-            // Set weight to 40%.
-            meteringAreas.add(new Camera.Area(areaRect2, 400));
-
-            params.setMeteringAreas(meteringAreas);
-        }
-
-        mCamera.setParameters(params);*/
+        });
     }
 
     @Override
@@ -111,7 +79,7 @@ public class ColorMeThis extends Activity implements
 
     @Override
     public boolean onFling(MotionEvent event1, MotionEvent event2,
-                           float velocityX, float velocityY) {
+                               float velocityX, float velocityY) {
         Log.d(DEBUG_TAG, "onFling: " + event1.toString()+event2.toString());
         return true;
     }
@@ -123,7 +91,7 @@ public class ColorMeThis extends Activity implements
 
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-                            float distanceY) {
+                                float distanceY) {
         Log.d(DEBUG_TAG, "onScroll: " + e1.toString()+e2.toString());
         return true;
     }
@@ -159,36 +127,6 @@ public class ColorMeThis extends Activity implements
         Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
         return true;
     }
-    private Preview mPreview;
-    Camera mCamera;
-    int numberOfCameras;
-    int cameraCurrentlyLocked;
-
-    // The first rear facing camera
-    int defaultCameraId;
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // Open the default i.e. the first rear facing camera.
-        mCamera = Camera.open();
-        cameraCurrentlyLocked = defaultCameraId;
-        mPreview.setCamera(mCamera);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        // Because the Camera object is a shared resource, it's very
-        // important to release it when the activity is paused.
-        if (mCamera != null) {
-            mPreview.setCamera(null);
-            mCamera.release();
-            mCamera = null;
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -209,7 +147,7 @@ public class ColorMeThis extends Activity implements
             /**
              case R.id.palette:
              return true;
-             */
+            */
         }
         return false;
     }
@@ -217,249 +155,111 @@ public class ColorMeThis extends Activity implements
     /** For SETTINGS: adjustments to be made at a later time.
      *  Files to check: Settings.java, preferences.xml
      *
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == RESULT_CANCELED) {
-            ...
+     @Override
+     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+         if (requestCode == RESULT_CANCELED) {
+             ...
+         }
+     }
+     */
+
+    /**
+     * Helper method to access the camera returns null if it cannot get the
+     * camera or does not exist
+     *
+     * @return
+     */
+    private Camera getCameraInstance() {
+        Camera camera = null;
+        try {
+            camera = Camera.open();
+        } catch (Exception e) {
+            // cannot get camera or does not exist
         }
+        return camera;
     }
-    */
 
-    private PictureCallback mPicture = new PictureCallback() {
-
+    PictureCallback mPicture = new PictureCallback() {
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
-            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-
+            File pictureFile = getOutputMediaFile();
             if (pictureFile == null) {
                 return;
             }
-
             try {
                 FileOutputStream fos = new FileOutputStream(pictureFile);
                 fos.write(data);
                 fos.close();
             } catch (FileNotFoundException e) {
-                Log.d(TAG, "File not found: " + e.getMessage());
+
             } catch (IOException e) {
-                Log.d(TAG, "Error accessing file: " + e.getMessage());
             }
         }
     };
 
-    // Add a listener to the Capture button.
-    View.OnClickListener mCaptureClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            // Get an image from the camera.
-            mCamera.takePicture(null, null, mPicture);
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                    "MyCameraApp");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
         }
-    };
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timeStamp + ".jpg");
 
-    /** Camera components:
-     * http://developer.android.com/guide/topics/media/camera.html#custom-camera
-    */
-
-    /** Detecting camera hardware: check if this device has a camera. */
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY))
-            return true;
-        else
-            return false;
+        return mediaFile;
     }
-
-    /** Accessing cameras: a safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance() {
-        Camera c = null;
-
-        try {
-            c = Camera.open();  // Attempt to get a Camera instance
-        } catch (Exception e) {
-            // Camera is not available (in use or does not exist)
-        }
-
-        return c; // Returns null if camera is not available
-    }
+}
 
     /** Checking camera features:
      *  Camera.getParameters()
      *  Camera.getCameraInfo()
      */
 
-    /** Creating a preview class: a basic camera preview class. */
-    class Preview extends ViewGroup implements SurfaceHolder.Callback {
-        private final String TAG = "Preview";
+class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mSurfaceHolder;
+    private Camera mCamera;
 
-        SurfaceView mSurfaceView;
-        SurfaceHolder mHolder;
-        Size mPreviewSize;
-        List<Size> mSupportedPreviewSizes;
-        Camera mCamera;
+    // Constructor that obtains context and camera
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        this.mCamera = camera;
+        this.mSurfaceHolder = this.getHolder();
+        this.mSurfaceHolder.addCallback(this);
+        this.mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
 
-        Preview(Context context) {
-            super(context);
-
-            mSurfaceView = new SurfaceView(context);
-            addView(mSurfaceView);
-
-            // Install a SurfaceHolder.Callback so we get notified when the
-            // underlying surface is created and destroyed.
-            mHolder = mSurfaceView.getHolder();
-            mHolder.addCallback(this);
-            mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        }
-
-        public void setCamera(Camera camera) {
-            mCamera = camera;
-            mCamera.setDisplayOrientation(90);
-            if (mCamera != null) {
-                mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
-                requestLayout();
-            }
-        }
-
-        @Override
-        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-            // We purposely disregard child measurements because act as a
-            // wrapper to a SurfaceView that centers the camera preview instead
-            // of stretching it.
-            final int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
-            final int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
-            setMeasuredDimension(width, height);
-
-            if (mSupportedPreviewSizes != null) {
-                mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes, width, height);
-            }
-        }
-
-        @Override
-        protected void onLayout(boolean changed, int l, int t, int r, int b) {
-            if (changed && getChildCount() > 0) {
-                final View child = getChildAt(0);
-
-                final int width = r - l;
-                final int height = b - t;
-
-                int previewWidth = width;
-                int previewHeight = height;
-
-                // Center the child SurfaceView within the parent.
-                if (width * previewHeight > height * previewWidth) {
-                    final int scaledChildWidth = previewWidth * height / previewHeight;
-                    child.layout((width - scaledChildWidth) / 2, 0,
-                            (width + scaledChildWidth) / 2, height);
-                } else {
-                    final int scaledChildHeight = previewHeight * width / previewWidth;
-                    child.layout(0, (height - scaledChildHeight) / 2,
-                            width, (height + scaledChildHeight) / 2);
-                }
-            }
-        }
-
-        public void surfaceCreated(SurfaceHolder holder) {
-            // The Surface has been created, acquire the camera and tell it where
-            // to draw.
-            try {
-                if (mCamera != null) {
-                    mCamera.setPreviewDisplay(holder);
-                }
-            } catch (IOException exception) {
-                Log.e(TAG, "IOException caused by setPreviewDisplay()", exception);
-            }
-        }
-
-        public void surfaceDestroyed(SurfaceHolder holder) {
-            // Surface will be destroyed when we return, so stop the preview.
-            if (mCamera != null) {
-                mCamera.stopPreview();
-            }
-        }
-
-        private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
-            final double ASPECT_TOLERANCE = 0.1;
-            double targetRatio = (double) w / h;
-            if (sizes == null) return null;
-
-            Size optimalSize = null;
-            double minDiff = Double.MAX_VALUE;
-
-            int targetHeight = h;
-
-            // Try to find an size match aspect ratio and size
-            for (Size size : sizes) {
-                double ratio = (double) size.width / size.height;
-                if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
-                if (Math.abs(size.height - targetHeight) < minDiff) {
-                    optimalSize = size;
-                    minDiff = Math.abs(size.height - targetHeight);
-                }
-            }
-
-            // Cannot find the one match the aspect ratio, ignore the requirement
-            if (optimalSize == null) {
-                minDiff = Double.MAX_VALUE;
-                for (Size size : sizes) {
-                    if (Math.abs(size.height - targetHeight) < minDiff) {
-                        optimalSize = size;
-                        minDiff = Math.abs(size.height - targetHeight);
-                    }
-                }
-            }
-            return optimalSize;
-        }
-
-        public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-            // Now that the size is known, set up the camera parameters and begin
-            // the preview.
-            Camera.Parameters parameters = mCamera.getParameters();
-            parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
-            requestLayout();
-
-            mCamera.setParameters(parameters);
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        try {
+            mCamera.setPreviewDisplay(surfaceHolder);
             mCamera.startPreview();
+        } catch (IOException e) {
+            // left blank for now
         }
     }
 
-    /** Saving media files */
-    public static final int MEDIA_TYPE_IMAGE = 1;
-
-    /** Create a file Uri for saving an image. */
-    public static Uri getOutputMediaFileUri(int type) {
-        return Uri.fromFile(getOutputMediaFile(type));
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        mCamera.stopPreview();
+        mCamera.release();
     }
 
-    /** Create a File for saving an image. */
-    private static File getOutputMediaFile(int type) {
-        // To be safe, check that the SD Card is mounted using
-        // Environment.getExternalStorageState() before doing this.
-
-        // This location works best if you want the created images to be shared between
-        // applications and persist after the app has been uninstalled.
-
-        File mediaStorageDir = new File
-                (Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                        "ColorMeThis");
-
-        // Create the storage directory if it does not exist.
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
-                Log.d("ColorMeThis", "failed to create directory.");
-                return null;
-            }
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+        mCamera.setDisplayOrientation(90);
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(surfaceHolder);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            // intentionally left blank for a test
         }
-
-        // Create a media file name.
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File mediaFile;
-
-        if (type == MEDIA_TYPE_IMAGE) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" +
-                    timeStamp + ".jpg");
-        }
-
-        else
-            return null;
-
-        return mediaFile;
     }
 }
